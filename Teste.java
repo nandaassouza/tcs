18fa57610accbfc755b1f31cdaada9d7cde8ff50
nandaassouza <52044332+nandaassouza@users.noreply.github.com>
2019-08-06 16:04:28 -0300

    Add files via upload

---
+++ Teste.java
@@ -0,0 +1,18 @@
+
+package exercicio1;
+
+public class Teste {
+    
+//    public static void main(String[] args){
+//        for (int i = 0; i <= 10; i++) {
+//            if (i%2==0) {
+//                System.out.println("Par: "+i);
+//            }else{
+//                System.out.println("Ímpar: "+i);
+//            }
+//        }
+//    }
+//    https://kubernetes.io/docs/tasks/access-application-cluster/service-access-application-cluster/
+//    https://kubernetes.io/docs/tasks/tools/install-kubectl/
+    
+}
